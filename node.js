0e7369bdc2a0296f9d948d2a6c82a8b98ea39d4f
Agustinho Neto <agustinho.pneto@gmail.com>
2023-03-26 22:08:49 -0300

    add rule

--- node.js
+++ node.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'eslint-plugin-import-helpers'],
   rules: {
+    'no-useless-constructor': 'off',
     'prettier/prettier': [
       'error',
       {
